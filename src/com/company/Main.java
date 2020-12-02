33df8fe4c3f57f76cb80d6c71894200f147fd2b8
jadekang <jkang.se@gmail.com>
2020-12-01 19:44:54 -0800

    Created Animal class and Dog which inherits the Animal class.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,22 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Dog dog = new Dog();
+	    dog.makeSound();
+    }
+}
+
+class Animal {
+    String sound = "";
+
+    void makeSound() {
+        System.out.println(sound);
+    }
+
+}
+
+class Dog extends Animal {
+    Dog() {
+        sound = "bark";
     }
 }
